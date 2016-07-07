ed4be97cb4d3eb2f64379f61f20dba1a0e1394b8
aaron beavers <aaron@debian.aaron.kettlelogic.space>
2016-07-07 14:28:53 -0400

    both gear constants changed back to 36

--- backend/signal/processor.js
+++ backend/signal/processor.js
@@ -5,10 +5,8 @@ const LinkedList = require("../../lib/DoublyLinkedList.js").DoublyLinkedList;
 const math = require('../util/math.js');
 const util = require('../util/util.js');
 
-const g = 6;
-const cC = 18.849;
-var sight = cC - (cC * 0.9);
-var C = cC;
+const g = 36;
+const C = 18.849;
 const di = C / g;
 
 class Processor extends EventEmitter {
